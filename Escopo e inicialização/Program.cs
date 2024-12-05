6d183bf9c1992c4f1ea50dadad3ac54268928fd1
Evandro2270 <evandro535@gmail.com>
2024-12-05 11:19:14 -0300

    Adicionar arquivos de projeto.

---
+++ Escopo e inicialização/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                double desconto = 0.0;
+                double preco = double.Parse(Console.ReadLine());
+
+                if (preco > 100.0)
+                {
+                    desconto = preco * 0.1;
+                }
+                Console.WriteLine(desconto);
+
+            }
+        }
+    }
+}
